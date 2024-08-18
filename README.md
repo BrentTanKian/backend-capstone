9f90241c1d66817ef611fdb1a292dc06145f77a9
unknown <brentyly@gmail.com>
2024-08-18 18:47:50 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# backend-developer-capstone
+This repo/project is the capstone project of the Meta backend developer professional certificate course
---
+++ littlelemon/restaurant/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from .models import Menu, Booking
+from .serializers import MenuItemSerializer, BookingSerializer
+from rest_framework.viewsets import ModelViewSet
+
+
+def index(request):
+    return render(request, 'index.html', {})
+
+
+class MenuItemsView(ListCreateAPIView):
+    """
+    Handles GET (list all menu items) and POST (create a new menu item) requests.
+    """
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    """
+    Handles GET (retrieve a single menu item), PUT (update a menu item),
+    and DELETE (delete a menu item) requests.
+    """
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    """
+    A viewset that provides the standard actions for the Booking model.
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
